8506046ed794b0f2d54bc38427af6ff81f4e3d64
moalamir52 <mo.alamir52@gmail.com>
2025-09-20 13:31:28 +0400

    Clean project - Remove old files and fix cache issues

--- auth-check.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    window.addEventListener('load', function() {
-        const isAuthenticated = localStorage.getItem('isAuthenticated');
-        if (!isAuthenticated || isAuthenticated !== 'true') {
-            window.location.href = 'https://moalamir52.github.io/Yelo/';
-        }
-    });
-})();
